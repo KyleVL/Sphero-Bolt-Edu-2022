8c2e182298563bf5faf0e04ba67d4f6cbeeaffed
Kyle <02kyle.vanlandegent@gulllakecs.org>
2022-01-13 09:11:04 -0800

    Square stable (Triangle commented out)

--- Sandbox.js
+++ Sandbox.js
@@ -5,13 +5,28 @@ Program: This is a sandbox where I learn
 */
 async function startProgram() {
 	// Write code here
-	setHeading(270);
+	/*setHeading(270);
 	await speak('I am donkey!', true);
 	setMainLed({ r: 255, g: 0, b: 0});
 		for (var _i1 = 0; _i1 < 3; _i1++) {
 			await Sound.Animal.Donkey.play(true);
 			setMainLed({ r: 0, g: 255, b: 0});
 			await roll((getHeading() + 120), 50, 2);
+			setMainLed({ r: 255, g: 0, b: 0});
+			await delay (1);
+		
+		}
+	await Sound.Effects.Applause(true);
+	
+}*/
+
+setHeading(270);
+	await speak('I am donkey!', true);
+	setMainLed({ r: 255, g: 0, b: 0});
+		for (var _i1 = 0; _i1 < 4; _i1++) {
+			await Sound.Animal.Donkey.play(true);
+			setMainLed({ r: 0, g: 255, b: 0});
+			await roll((getHeading() + 90), 50, 2);
 			setMainLed({ r: 255, g: 0, b: 0});
 			await delay (1);
 		
