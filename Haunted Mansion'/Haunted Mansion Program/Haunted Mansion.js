cbcf9cac0a4a2746240a03644c6964c0b3e25268
Kyle <02kyle.vanlandegent@gulllakecs.org>
2022-01-31 09:46:00 -0800

    Kyle- Red light Challenge

--- Haunted Mansion'/Haunted Mansion Program/Haunted Mansion.js
+++ Haunted Mansion'/Haunted Mansion Program/Haunted Mansion.js
@@ -13,8 +13,11 @@ async function startProgram() {
 	await roll((getHeading() + 90), 67, 2);
 	//Chalenge 2
 	await Sound.Animal.SeaLion.play(true);
-	await roll((getHeading() + 90), 40, 2);
+	await roll((getHeading() + 90), 43, 2);
+	//Chalenge 3
+	await roll((getHeading() + 45), 40, 2);
+	await roll((getHeading() + 270), 24, 2);
+	setMainLed ({ r: 255, g: 0, b: 0});
 }
 	
-
 
