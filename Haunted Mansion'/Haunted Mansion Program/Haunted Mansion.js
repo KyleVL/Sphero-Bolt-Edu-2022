ef885af6a539d9e419bc4d9ae1e16952aedde0b8
Kyle <02kyle.vanlandegent@gulllakecs.org>
2022-01-31 09:30:09 -0800

    Nolan - First noise challenge

--- Haunted Mansion'/Haunted Mansion Program/Haunted Mansion.js
+++ Haunted Mansion'/Haunted Mansion Program/Haunted Mansion.js
@@ -7,7 +7,14 @@ async function startProgram() {
 	// Write code here
 	setHeading(270);
 	await speak('Start');
-	await roll ((getHeading() + 90), 73,2);
+	// Challenge 1
+	await roll ((getHeading() + 90), 72,2);
 	setMainLed ({ r: 0, g: 0, b: 255});
-	await roll((getHeading() + 90), 52, 2);
+	await roll((getHeading() + 90), 67, 2);
+	//Chalenge 2
+	await Sound.Animal.SeaLion.play(true);
+	await roll((getHeading() + 90), 40, 2);
 }
+	
+
+
