bf5363df969b2a90c0f59ada81e0404d7af31c60
Kyle <02kyle.vanlandegent@gulllakecs.org>
2022-01-31 09:17:29 -0800

    Kyle-Blue Light + Turn 90 degree

--- Haunted Mansion'/Haunted Mansion Program/Haunted Mansion.js
+++ Haunted Mansion'/Haunted Mansion Program/Haunted Mansion.js
@@ -3,4 +3,11 @@ Programer: Kyle VanLandegent
 Team Member: Nolan Loukes
 Program: Maze
 */
-
+async function startProgram() {
+	// Write code here
+	setHeading(270);
+	await speak('Start');
+	await roll ((getHeading() + 90), 73,2);
+	setMainLed ({ r: 0, g: 0, b: 255});
+	await roll((getHeading() + 90), 52, 2);
+}
